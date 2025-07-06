066164eb3db7aab11a1495ec07daab38c1ed1a51
Tochiyeah <afablejrchito@gmail.com>
2025-07-06 23:07:13 +0800

    Updated dockerfile and removed start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Add library path for libzbar
-export LD_LIBRARY_PATH=/usr/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-
-# Run Gunicorn with Eventlet worker
-exec gunicorn --worker-class eventlet --bind 0.0.0.0:5000 run:app
